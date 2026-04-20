376b9c8f8afb08bc1a09183913bd582a984edd36
Ilia Lugovoi <121395392+ilia-lugovoi@users.noreply.github.com>
2026-04-20 17:09:37 +0300

    add pandas

--- airflow/dags/conect_clickhouse.py
+++ airflow/dags/conect_clickhouse.py
@@ -1,3 +1,7 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import pyodbc
 import clickhouse_connect
 
 def mssql_to_clickhouse():
